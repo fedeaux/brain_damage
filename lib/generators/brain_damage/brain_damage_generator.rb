a9f83f60cdcaadaac424f668cf025f85862066ca
Pedro Bernardes <ph@everywhere.com.br>
2016-03-01 21:44:55 -0300

    Reworking from scratch

---
+++ lib/generators/brain_damage/brain_damage_generator.rb
@@ -0,0 +1,5 @@
+require 'rails/generators/rails/scaffold/scaffold_generator'
+
+class BrainDamageGenerator < Rails::Generators::ScaffoldGenerator
+  source_root File.expand_path('../templates', __FILE__)
+end
