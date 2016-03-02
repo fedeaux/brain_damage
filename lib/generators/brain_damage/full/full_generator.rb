6855f324a99c80892be8680192b78ec4d9534737
Pedro Bernardes <ph@everywhere.com.br>
2016-03-02 17:59:40 -0300

    Adding full generator

---
+++ lib/generators/brain_damage/full/full_generator.rb
@@ -0,0 +1,27 @@
+module BrainDamage
+  class FullGenerator < Rails::Generators::Base
+    class_option :descriptions, desc: "The .rb files", :optional => true, :default => '*'
+    class_option :destroy, desc: "Destroy instead of creat", :optional => true, :default => false
+
+    def just_do_it
+      action = options[:destroy] ? 'd' : 'g'
+
+      descriptions = if options[:descriptions] == '*'
+        then
+          Dir["description/*"].map { |description|
+            if description[-3, 3] == '.rb' then
+              description.split('/').last
+            else
+              nil
+            end
+          }.reject(&:nil?)
+      else
+        options[:descriptions].split(',').map(&:strip)
+      end
+
+      descriptions.each do |description|
+        system "rails #{action} brain_damage:resource --description=#{description}"
+      end
+    end
+  end
+end
