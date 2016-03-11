139abcbed8d9733347ae39e2383df9b724c3718a
Pedro Bernardes <ph@everywhere.com.br>
2016-03-11 12:35:11 -0300

    Major cleanup

---
+++ description/006.email.rb
@@ -0,0 +1,35 @@
+@name = 'Email'
+
+@fields = {
+  email: :string,
+  name: :string,
+  owner: :'references{polymorphic}'
+}
+
+set_view_schema(:type => :single_page_manager, :special_views => ['_nested.fields'])
+
+describe_field :owner, {
+  relation: {
+    type: :belongs_to,
+  },
+
+  input: {
+    type: :polymorphic_select,
+    options: {
+      options: [
+        { model: :Contact, display: :name, value: :id },
+        # { model: :Local, display: :name, value: :id },
+        # { model: :User, display: :name, value: :id },
+      ]
+    }
+  },
+
+  display: {
+    type: :link_to,
+    options: {
+      method: :owner,
+      display: :name,
+      null: :ignore
+    }
+  }
+}
---
+++ description/007.phone.rb
@@ -0,0 +1,39 @@
+@name = 'Phone'
+
+@fields = {
+  ddi: :string,
+  ddd: :string,
+  prefix: :string,
+  suffix: :string,
+  ramal: :ddd,
+  name: :string,
+  owner: :'references{polymorphic}'
+}
+
+set_view_schema(:type => :single_page_manager, :special_views => ['_nested.fields'])
+
+describe_field :owner, {
+  relation: {
+    type: :belongs_to,
+  },
+
+  input: {
+    type: :polymorphic_select,
+    options: {
+      options: [
+        { model: :Contact, display: :name, value: :id },
+        # { model: :Local, display: :name, value: :id },
+        # { model: :User, display: :name, value: :id },
+      ]
+    }
+  },
+
+  display: {
+    type: :link_to,
+    options: {
+      method: :owner,
+      display: :name,
+      null: :ignore
+    }
+  }
+}
